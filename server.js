d38c9180fff17c3dabcb059bf69ec2d3cf0d5be9
Caleb Luster <cwl117@outlook.com>
2022-03-07 16:19:26 -0600

    using my good buddies at stack overflow I found a nice app.get functions and the require util to print formatted strings

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-
+const util = require("util");
 const app = express();
 const PORT = process.env.PORT || 3003;
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+
+app.get("/api/notes", function (req, res) {});
 
 app.listen(PORT, function () {
   console.log(`Live server launched on PORT ${PORT}!`);
