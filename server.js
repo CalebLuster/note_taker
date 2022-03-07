7e1686424844742e83b34cf7b7a9bed453d1cea5
Caleb Luster <cwl117@outlook.com>
2022-03-07 16:30:12 -0600

    added the app.get function. Someone on stack overflow also helped me with this. This module is very confusing and I feel like I am not making much progress

--- server.js
+++ server.js
@@ -7,7 +7,15 @@ const PORT = process.env.PORT || 3003;
 const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
 
-app.get("/api/notes", function (req, res) {});
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/api/notes", function (req, res) {
+  readFile("./Develop/db/db.json", "utf8").then(function (data) {
+    notes = [].concat(JSON.parse(data));
+    res.json(notes);
+  });
+});
 
 app.listen(PORT, function () {
   console.log(`Live server launched on PORT ${PORT}!`);
