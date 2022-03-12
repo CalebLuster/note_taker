675a6edf237b4f89b768b03fecc57e246b04df46
Caleb Luster <cwl117@outlook.com>
2022-03-11 19:12:54 -0600

    I cannot find the issue I might just end up rewriting some of the route paths, my instructor suggested putting the routes in a different folder so I will probably try that.

--- server.js
+++ server.js
@@ -10,41 +10,42 @@ const writeFile = util.promisify(fs.writeFile);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("./Develop/public"));
+app.use(express.static("./develop/public"));
 
 app.get("/api/notes", function (req, res) {
-  readFile("./Develop/db/db.json", "utf8").then(function (data) {
+  readFile("./develop/db/db.json", "utf8").then(function (data) {
     notes = [].concat(JSON.parse(data));
     res.json(notes);
   });
 });
 
-app.post("api/notes", (req, res) => {
+app.post("api/notes", function (req, res) {
   const note = req.body;
-  readFile("./Develop/db/db.json", "utf8")
+  readFile("./develop/db/db.json", "utf8")
     .then(function (data) {
       const notes = [].concat(JSON.parse(data));
       note.id = notes.length + 1;
       notes.push(note);
+      return notes;
     })
     .then(function (notes) {
-      writeFile("./Develop/db/db.json", JSON.stringify(notes));
+      writeFile("./develop/db/db.json", JSON.stringify(notes));
       res.json(note);
     });
 });
 
 app.get("/notes", function (req, res) {
-  res.sendFile(path.join(__dirname, "./Develop/public/notes.html"));
+  res.sendFile(path.join(__dirname, "./develop/public/notes.html"));
 });
 
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
+  res.sendFile(path.join(__dirname, "./develop/public/index.html"));
 });
 
 app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
+  res.sendFile(path.join(__dirname, "./develop/public/index.html"));
 });
-
+ 
 app.listen(PORT, function () {
   console.log(`Live server launched on PORT ${PORT}!`);
 });
