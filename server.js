366a6d33881175be5b09524283b2c3e8e1c544f6
Caleb Luster <cwl117@outlook.com>
2022-03-11 19:29:02 -0600

    I believe I fixed the server.js. For now everything is commented out in case I need to go back but I belive this the direction my instructor wanted me to go.

--- server.js
+++ server.js
@@ -1,51 +1,52 @@
 const express = require("express");
-const path = require("path");
-const fs = require("fs");
-const util = require("util");
 const app = express();
 const PORT = process.env.PORT || 8000;
-const readFile = util.promisify(fs.readFile);
-const writeFile = util.promisify(fs.writeFile);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("./Develop/public"));
 
-app.get("/api/notes", function (req, res) {
-  readFile("./Develop/db/db.json", "utf8").then(function (data) {
-    notes = [].concat(JSON.parse(data));
-    res.json(notes);
-  });
-});
-
-app.post("api/notes", function (req, res) {
-  const note = req.body;
-  readFile("./Develop/db/db.json", "utf8")
-    .then(function (data) {
-      const notes = [].concat(JSON.parse(data));
-      note.id = notes.length + 1;
-      notes.push(note);
-      return notes;
-    })
-    .then(function (notes) {
-      writeFile("./Develop/db/db.json", JSON.stringify(notes));
-      res.json(note);
-    });
-});
-
-app.get("/notes", function (req, res) {
-  res.sendFile(path.join(__dirname, "./Develop/public/notes.html"));
-});
-
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
-});
-
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
-});
- 
 app.listen(PORT, function () {
   console.log(`Live server launched on PORT ${PORT}!`);
 });
+
+// const readFile = util.promisify(fs.readFile);
+// const writeFile = util.promisify(fs.writeFile);
+
+// const path = require("path");
+// const fs = require("fs");
+// const util = require("util");
+
+// app.get("/api/notes", function (req, res) {
+//   readFile("./Develop/db/db.json", "utf8").then(function (data) {
+//     notes = [].concat(JSON.parse(data));
+//     res.json(notes);
+//   });
+// });
+
+// app.post("api/notes", function (req, res) {
+//   const note = req.body;
+//   readFile("./Develop/db/db.json", "utf8")
+//     .then(function (data) {
+//       const notes = [].concat(JSON.parse(data));
+//       note.id = notes.length + 1;
+//       notes.push(note);
+//       return notes;
+//     })
+//     .then(function (notes) {
+//       writeFile("./Develop/db/db.json", JSON.stringify(notes));
+//       res.json(note);
+//     });
+// });
+
+// app.get("/notes", function (req, res) {
+//   res.sendFile(path.join(__dirname, "./Develop/public/notes.html"));
+// });
+
+// app.get("/", function (req, res) {
+//   res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
+// });
+
+// app.get("*", function (req, res) {
+//   res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
+// });
